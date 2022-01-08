0c0dedb48bebe52d23cf95fd5a9586b3f12024d9
Biplab Pati <89262168+Biplab61@users.noreply.github.com>
2022-01-08 13:13:20 +0530

    Update biplab.js

--- biplab-pati-db/biplab.js
+++ biplab-pati-db/biplab.js
@@ -42,12 +42,14 @@
             document.write("<center><h1>Comming Soon!<h1></center>")
          }
 
-         function dis(){
-             document.getElementById("disable").disabled = true;
-             setTimeout(function(){
-                document.getElementById("disable").disabled = false;
-             }, 10000);
-         }
+          function dis(){
+            setTimeout(function(){
+                document.getElementById("disable").disabled = true;
+            },500);
+            setTimeout(function(){
+               document.getElementById("disable").disabled = false;
+            }, 20000);
+        }
          
          
                   //   Date Checker and Picker Js
